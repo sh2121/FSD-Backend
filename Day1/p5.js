a44650b8827df99ddfa93260f49c1e80b67ac3ee
sh2121 <shreyanshch07@gmail.com>
2025-02-17 12:58:10 +0530

    Directory Create and Delete

--- Day1/p5.js
+++ Day1/p5.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const write=() =>{
-    const data = "I am updated data";
-    fs.writeFile()("./data.txt",data,(err) =>{
+    const data = "I am new data";
+    fs.writeFile("./mydir/data.txt",data,(err) =>{
         if(err)
             console.log(err);
         else 
